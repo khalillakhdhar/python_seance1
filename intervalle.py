13e6566f2b5545cc95d498e717494ddf05f5822f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-02-25 14:22:47 +0100

    Update intervalle.py

--- intervalle.py
+++ intervalle.py
@@ -1,9 +1,5 @@
 a=1240
-if a<100:
-    print("a est un petit nom")
-elif a<1000:
-    print("a est un nombre moyen")
-elif a<10000:
-    print("a est un grand nombre")
-else:
-    print("a est trés grand")
+if a<100:   print("a est un petit nom")
+elif a<1000:    print("a est un nombre moyen")
+elif a<10000:   print("a est un grand nombre")
+else:   print("a est trés grand")
