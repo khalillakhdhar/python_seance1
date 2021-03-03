5fea2df48bbb271cc7b873b4d65c43f6df02e0a7
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-03 16:07:36 +0100

    data

--- databases/datatest.py
+++ databases/datatest.py
@@ -1,13 +1,28 @@
 import mysql.connector
+#pip install mysql-connector
 mydb = mysql.connector.connect(
 host="localhost",
-user="root",
-passwd="",
+user="root", #username par defaut
+passwd="", #password par defaut
 database="sante"
 )
-mycursor = mydb.cursor()
-sql = "INSERT INTO `personne`( `nom`, `age`, `etat`) VALUES (%s, %s,%s)"
-val = ("khalil",30, "acceptable")
-mycursor.execute(sql, val)
-mydb.commit()
-print(mycursor.rowcount, "record inserted.")
+nom=""
+age=0
+etat=""
+decision=""
+while decision!="o":
+    while age<12:
+        age=(int(input("donner votre age")))
+    while nom=="":
+        nom=input("donner votre nom")
+    while etat=="":
+        etat=input("donner votre état de santé")
+
+    # connexion mysql avec python
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO `personne`( `nom`, `age`, `etat`) VALUES (%s, %s,%s)"
+    val = (nom,age, etat)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
+    decision=input("arrêter ? (O pour oui)" )
