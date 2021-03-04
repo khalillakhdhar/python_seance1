987c28562d5dcaf31428ae221d981e40fe9a07d2
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-04 16:11:09 +0100

    Update personne.py

--- databases/classes/personne.py
+++ databases/classes/personne.py
@@ -2,5 +2,20 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+    def test(self):
+        if(self.age<18):
+            print("%s est mineur"%(self.name))
+        else:
+            print("%s est majeur"%self.name)
+
+
+"""
 p1 = Person("John", 36)
 print(p1.name)
+p2=Person("Alaa",17)
+p2.test()
+p1.test()
+"""
+
+
