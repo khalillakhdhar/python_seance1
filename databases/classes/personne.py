09acb3afed2cd443b00fd876602abd63a1dd8b2e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-04 16:06:22 +0100

    Update personne.py

--- databases/classes/personne.py
+++ databases/classes/personne.py
@@ -1,6 +1,6 @@
 class Person:
- def __init__(self, name, age):
- self.name = name
- self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 p1 = Person("John", 36)
 print(p1.name)
