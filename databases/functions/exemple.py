571deadcfffe918c51d2a79a860730b98750323e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-04 14:29:04 +0100

    Update exemple.py

--- databases/functions/exemple.py
+++ databases/functions/exemple.py
@@ -1,6 +1,6 @@
 def ctr(valeur):
     while valeur<1:
-        valeur=input("donner un entier positif")
+        valeur=int(input("donner un entier positif"))
     return valeur
 
 def parite(x):
@@ -8,3 +8,16 @@ def parite(x):
         print("paire")
     else:
         print("impaire")
+def fact(y):
+    f=1
+    for i in range(2,y+1):
+        f=f*i
+    print("la factoriel de %s est %s"%(y,f))
+
+x=0
+y=0
+valeur=0
+x=ctr(x)
+parite(x)
+y=ctr(y)
+fact(y)
