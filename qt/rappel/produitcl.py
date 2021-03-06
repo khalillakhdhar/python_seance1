4398775772f2043bac7942bb915a573e7cc8c850
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-06 09:09:54 +0100

    Update produitcl.py

--- qt/rappel/produitcl.py
+++ qt/rappel/produitcl.py
@@ -1,2 +1,14 @@
 class produit:
-    def __init__(self,prixu,prixv,quantite):
+    def __init__(self,titre,prixu,prixv,quantite):
+        self.titre=titre
+        self.prixu=prixu
+        self.prixv=prixv
+        self.quantite=quantite
+    def gain_brute(self):
+        gain=(self.prixv-self.prixu)*self.quantite
+        return gain
+
+#programme de test
+pr1=produit("MSI gamer",3700,4100,2)
+chiffre=pr1.gain_brute()
+print("votre chiffre de gain estimé est de : %s"%(chiffre))
