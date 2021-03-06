21d6da351422183bc70a3dbdb80879081e0880c4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-06 10:51:45 +0100

    call to class

---
+++ qt/rappel/produitcode.py
@@ -0,0 +1,6 @@
+from produitcl import *
+rep=int(input("donner le nombre de produits à ajouter" ))
+for i in range(rep):
+    produit1=produit("",0,0,0)
+    produit1.read()
+    produit1.detailles()
