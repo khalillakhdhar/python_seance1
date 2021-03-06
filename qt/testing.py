5e051198ff60429e87e840750f38df684183bf62
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-06 07:53:44 +0100

    texte

---
+++ qt/testing.py
@@ -0,0 +1,24 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QDialog, QTableWidgetItem,QTextEdit
+from test import * # importation de code de l'interface
+class welcome(QDialog): #(QDialog) permet l'héritage de QDialog
+    def __init__(self):
+        super().__init__() # applé le constructeur de l'interface
+        self.ui=Ui_Dialog()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.tester)
+        self.show()
+    def tester(self):
+        self.ui.valeur.setText(str(self.ui.nom.toPlainText()))
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+
+    w = welcome()
+
+    w.show()
+
+    sys.exit(app.exec_())
