54b265f231512d8a931f566db48d3d4b0f10abc8
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-06 10:42:38 +0100

    Update produitcl.py

--- qt/rappel/produitcl.py
+++ qt/rappel/produitcl.py
@@ -15,6 +15,15 @@ class produit:
             return self.prixv*0.09
         else:
             return self.prixv*0.12
+    def read(self):
+        while(self.titre==""):
+            self.titre=input("titre=? ")
+        while(self.prixu<1):
+            self.prixu=float(input("donner votre prix d'achat"))
+        while(self.prixv<1 or self.prixv<self.prixu):
+            self.prixv=float(input("donner le prix de vente"))
+        while(quantite<1):
+            self.quantite=int(input("donner la quantité"))
 
 
 #programme de test
@@ -25,3 +34,5 @@ print("votre chiffre de gain estimé est de : %s"%(chiffre))
 taxes=pr1.taxe()
 print("la taxe unitaire à payer est de %s"%(taxes))
 """
+produit1=produit("",0,0,0)
+produit1.read()
