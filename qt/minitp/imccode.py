3df8b2358cf0f093684d216eed3d9ed0ecd247e6
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-13 10:59:23 +0100

    Update imccode.py

--- qt/minitp/imccode.py
+++ qt/minitp/imccode.py
@@ -7,7 +7,23 @@ class IMC(QDialog): #(QDialog) permet l'héritage de QDialog
         super().__init__() # applé le constructeur de l'interface
         self.ui=Ui_Dialog()
         self.ui.setupUi(self)
+        self.ui.btn.clicked.connect(self.affichage)
         self.show()
+    def indicemc(self,taille,poids):
+        return taille/ (poids*poids)
+    def interpretation(self,t,p):
+        imcv=self.indicemc(t,p)
+        if(imcv<20):
+            return "maigre"
+        elif(imcv<=25):
+            return "ideal"
+        else:
+            return "surpoids"   
+    def affichage(self):
+        poid=float(self.ui.vpoid)
+
+
+
 
 
 
